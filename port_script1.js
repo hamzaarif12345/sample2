b66f45c0e57dab9455b7737f1494e1f1be36ba74
Hamza Arif <hamza81611@gmail.com>
2025-11-05 15:06:10 +0530

    first commit

--- port_script1.js
+++ port_script1.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
         
-        // scroll-up button show/hide script
+        // scroll-up button show/hide script 
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
